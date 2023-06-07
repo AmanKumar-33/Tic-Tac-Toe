20aaf364c1c899927170304015f2593425057f65
Aman Kumar <89511864+AmanKumar-33@users.noreply.github.com>
2023-06-07 10:57:28 +0530

    Add files via upload

---
+++ EnemyController.js
@@ -0,0 +1,27 @@
+export default class EnemyController{
+     enemyMap = [
+        [1,1,1,1,1,1,1,1,1,1],
+        [1,1,1,1,1,1,1,1,1,1],
+        [2,2,2,3,3,3,3,2,2,2],
+        [2,2,2,3,3,3,3,2,2,2],
+        [1,1,1,1,1,1,1,1,1,1],
+        [2,2,2,2,2,2,2,2,2,2], 
+     ];
+
+    constructor(canvas){
+        this.canvas = canvas;
+        this.createEnemies();
+    }
+    draw(ctx){
+
+        creatEnemies(){
+            this.enemyMap.forEach((row,rowIndex)=>{
+                this.enemyRows[rowIndex]=[];
+                this.
+                
+            
+        
+    
+
+    }
+}
